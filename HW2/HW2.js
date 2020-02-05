66e5b9d67bbcc5b388f6151f9116f0975deb5367
Maria <voloshkamasha@gmail.com>
2020-02-05 18:49:36 +0200

    Update HW2.js

--- HW2/HW2.js
+++ HW2/HW2.js
@@ -1,1 +1,30 @@
+let numberN = parseInt(prompt("Введіть число: "), 10);
+while (isNaN(numberN)) {
+  numberN = Math.round(parseInt(prompt("Введіть число: "), 10));
+}
+let numberM = parseInt(prompt("Введіть число: "), 10);
+while (isNaN(numberM)) {
+  numberM = Math.round(parseInt(prompt("Введіть число: "), 10));
+}
+const skipEvenNumbers = confirm("Пропустити парні числа?", false);
+let sum = 0;
+
+for (let i = numberN; i <= numberM; i++) {
+  if (skipEvenNumbers) {
+    if (i % 2 !== 0) {
+      sum += i;
+    }
+  } else {
+    sum += i;
+  }
+}
+
+console.log(sum);
+
+document.write(`
+Число N: ${numberN} </br>
+Число M: ${numberM} </br>
+Сума чисел: ${sum} </br>
+`)
+
 
