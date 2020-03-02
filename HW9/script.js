28f6e8a0f8468437cb77dcbaeddfa9daad4aae8d
Maria <voloshkamasha@gmail.com>
2020-03-02 18:10:47 +0200

    Create script.js

---
+++ HW9/script.js
@@ -0,0 +1,28 @@
+  let div = document.createElement("div");
+ function randomColor(){
+        let colorRes = document.querySelectorAll('#content div').forEach(el=>{
+          el.style.backgroundColor = '#'+Math.random().toString(14).slice(2,8);
+             });
+    return colorRes;
+}
+
+function generateBlocks(){
+    for(let i = 0; i < 25; i++){
+    let div = document.createElement("div");
+    div.style.background = randomColor();
+    content.append(div);
+    }
+}
+
+function generateBlocksInterval(){
+generateBlocks();
+    function repeatSquare(){
+      content.innerHTML = "";
+        generateBlocks();
+    }
+    setInterval(() => {
+        repeatSquare();
+    }, 1000);
+}
+
+generateBlocksInterval();
