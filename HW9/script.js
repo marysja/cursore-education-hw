44f84d6d752627f9e1c4466ba2a56b4d2839c69e
Maria <voloshkamasha@gmail.com>
2020-03-05 18:19:08 +0200

    Update script.js

--- HW9/script.js
+++ HW9/script.js
@@ -7,7 +7,7 @@
 }
 
 function generateBlocks(){
-    for(let i = 0; i < 25; i++){
+    for(let i = 0; i < 26; i++){
     let div = document.createElement("div");
     div.style.background = randomColor();
     content.append(div);
