b70e28d50f42b5d5d3a7544a229f034bf5570235
Maria <voloshkamasha@gmail.com>
2020-03-13 19:00:32 +0200

    Update HW8.js

--- HW8/HW8.js
+++ HW8/HW8.js
@@ -1,55 +1,53 @@
 class Student {
-  constructor(fullName, university, course, marks) {
-  this.fullName = fullName;
-  this.university = university;
-  this.course = course;
-  this.marks = marks;
+  constructor(university, course, fullName) {
+        this.university = university;
+        this.course = course;
+        this.fullName = fullName;
     }
-  
-
-getInfo() {
-  return `Студент:` + this.fullName + ` ` + this.university + ` `+ this.course;
+  getInfo() {
+  return `Студент: ` + this.fullName + ` ` + this.university + ` `+ this.course;
 }
-getMarks() {
-  return this.marks
-}
-setMark(mark){
-this.marks.push(mark);
-}
+ getMarks() {
+        return this.marks = [5, 4, 4, 5];
+    }
+ setMarks(val) {
+        this.marks.push(val);
+        return this.marks;
+    }
 getAverageMark(){
 return (this.marks.reduce((a, b) => (a + b), 0) / this.marks.length);
 }
-dismiss(){
-this.marks = null;
-return "Виключено";
+dismiss() {
+        return this.marks = null;
+    }
+    recover() {
+        return this.getMarks();
+    }
 }
-recover(){
-this.marks = [5, 4, 4, 5];
-return "Поновлено";
-}
-}
-const student = new Student(`Остап Родоманський Бендер`, `Высшей Школы Мошенничества г.Одесса`, 1, [5, 4, 3, 2]);
+let student = new Student(`Остап Родоманський Бендер`, `Высшей Школы Мошенничества г.Одесса`, 1, [5, 4, 3, 2]);
 console.log(`1. student`, student);
 console.log(`2. student.getInfo()`, student.getInfo());
 console.log('3. student.getMarks()', student.getMarks());
-console.log('4. student.setMark()', student.setMark());
+console.log('4. student.setMark()', student.setMarks());
 console.log('4. student.getAverageMark()', student.getAverageMark());
 console.log('4. student.dismiss()', student.dismiss());
 console.log('4. student.recover()', student.recover());
 
-/*class BudgetStudent extends Student {
-constructor(fullName, university, course, marks) {
-super(fullName, university, course, marks)
-getScholarship() {
-  if (this.marks === null || this.getAverageMark() < 4) {
-    return "Вы не получили 1400 грн. стипендии";
-  } else if (this.getAverageMark() > 4){
-    return "Вы получили 1400 грн. стипендии";
-  }
-}
+class BudgetStudent extends Student {
+    constructor(university, course, fullName) {
+        super(university, course, fullName);
+            }
+        getScholarship() {
+        this.getMarks();
+        if(this.marks === null || this.getAverageMark() < 4) {
+            return 'У Вас нет степендии'
+        } else {
+            return 'Вы получили 1400 грн. стипендии'
+        };
+    }
 }
 setInterval(()=> this.getScholarship(), 30000);
-let BudgetStudent = new BudgetStudent('Волошина Марія', 2, 'ЛНУ');
+let newStudent = new BudgetStudent('Волошина Марія', 2, 'ЛНУ');
 
-console.log(BudgetStudent.getUserInfo);
-console.log(BudgetStudent.getScholarship());
+console.log(newStudent);
+console.log(newStudent.getScholarship());
