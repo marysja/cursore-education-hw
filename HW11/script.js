cf9a7ad98d2288964e2b90ee01a4442f44590562
Maria <voloshkamasha@gmail.com>
2020-03-21 10:33:16 +0200

    Create script.js

---
+++ HW11/script.js
@@ -0,0 +1,20 @@
+function getSymbol() {
+    let sign = new Promise(num => {
+      setTimeout(() => {
+       +num(String.fromCharCode(Date.now().toString().slice(-5)))
+      }, 50)
+    })
+    return sign;
+  }
+  
+  async function getRandomChinese(length) {
+    let chineseSymbol = " ";
+    let i = 0;
+    while(i < length) {
+        chineseSymbol += await getSymbol();
+      i++;
+    }
+    return console.log(chineseSymbol);
+  }
+  
+  getRandomChinese(4);
